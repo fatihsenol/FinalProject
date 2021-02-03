037b68e75a44f40bda6fa322fc056a8447e4cf57
Fatih Şenol <fatihh.senoll123@gmail.com>
2021-02-03 23:47:24 +0300

    03.02.2021 Entity Framework ve Veri tabanı

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -9,18 +9,28 @@ namespace Business.Concrete
 {
     public class ProductManager : IProductService
     {
-        IProductDal _ProductDal;
+        IProductDal _productDal;
 
         public ProductManager(IProductDal productDal)
         {
-            _ProductDal = productDal;
+            _productDal = productDal;
         }
 
         public List<Product> GetAll()
         {
             //İş Kodları
             //Yetkisi Var mı? 
-            return _ProductDal.GetAll();
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
         }
     }
 }
